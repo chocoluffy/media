304d823edfa687029bcde89f51c521d22f88082c
Jiacheng Jiang <ajax15601560911@gmail.com>
2017-06-27 01:20:19 -0400

    Updated banner styles

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
         }
     };
     request(options, function (error, response, html) {
-        if (!error && response.statusCode == 200) { 
+        if (!error && response.statusCode == 200) {
             if (feedsrc.indexOf('weixin') > -1) { // if comes from weixin source.
                 var $ = cheerio.load(html);
                 // console.log(html);
@@ -27,78 +27,81 @@ app.get('/', function (req, res) {
                 });
 
                 $('body').append(
-                    `<div 
+                    `<div style="height: 49px; display:block; width:100%;" />
+                    <div
                         class="banner"
                         style="
-                            background-color:#262626;
+                            background-color:#fff;
                             width:100%;
-                            height:50px;
-                            box-shadow: 0px 1px 50px #5E5E5E;
+                            height:49px;
+                            box-shadow: 0px 0px 10px rgba(0, 0, 0, .15);
                             position:fixed;
                             bottom:0px;
                             "
-                     > 
+                     >
                         <div>
                             <img
                                  src="http://chocoluffy.com/media/server/static/icon_1024.png"
                                  style="
                                     position: absolute;
                                     bottom: 1em;
-                                    height: 60px;
-                                    width: 60px;
-                                    border-radius: 15px;
+                                    height: 40px;
+                                    width: 40px;
+                                    border-radius: 2px;
                                     margin-left: 1em;
                                     box-shadow: 0 1px 5px #A9DFFF, inset 0 0 3px;
                                 "
                             >
                         </div>
-                        <div 
+                        <div
                             style="
                                 display: flex;
                                 height: 100%;
-                                max-width: 51%;
                                 margin: 0 auto;
                                 align-items: center;
                                 justify-content: center;
-                                color: #fff;
+                                color: #262626;
                                 font-size: 0.82em;
-                                font-weight: 100;
+                                font-weight: 300;
                             "
                         >『鲸语』智能聚合本地最实用的资讯</div>
                         <div style="
                                     position: absolute;
-                                    border: 1px solid #A9DFFF;
-                                    border-radius: 5px;
-                                    bottom: 0.8em;
-                                    right: 1em;
+                                    background-color: #262626;
+                                    bottom: 0em;
+                                    height: 49px;
+                                    right: 0em;
                                     min-width: 20px;
                                     max-width: 70px;
                                     font-size: 0.82em;
-                                    display: block;
-                                    padding: 3px 9px;
+                                    display: flex;
+                                    padding-left: 1em;
+                                    padding-right: 1em;
                                     text-align: center;
+                                    justify-content: center;
+                                    align-items: center;
                             "
                         >
-                            <a 
+                            <a
                                 href="https://appsto.re/ca/klNuhb.i"
                                 style="
-                                    color: white;
-                                    font-weight: 100;
+                                    color: #fff;
+                                    font-weight: 300;
                                 "
                             >去试试</a>
                         </div>
                      </div>`
                 );
-                
+
                 res.send($.html());
             }
             else {
                 res.send("else");
-            } 
+            }
         };
     });
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})  
+})
